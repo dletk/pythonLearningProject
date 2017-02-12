2d25b1d1ba1a20ca69ab7aee035c35718f0798b0
dletk <dle@macalester.edu>
2017-02-11 19:07:41 -0600

    Commit for pitvotTable

--- Python for Data Science - Coursera/pivotTable.py
+++ Python for Data Science - Coursera/pivotTable.py
@@ -3,8 +3,13 @@ import numpy as np
 
 dataFrame_car = pd.read_csv("./data/cars.csv")
 
-print(dataFrame_car.head())
+# print(dataFrame_car.head())
 # print(dataFrame_car.set_index(["Make", "Size"]))
-groupby_make_size = dataFrame_car.groupby(["Make", "Size"])
+# groupby_make_size = dataFrame_car.groupby(["Make", "Size"])
 # print(groupby_make_size.groups)
-print(np.average(groupby_make_size.get_group(("TESLA", "FULL-SIZE"))["CITY (kWh/100 km)"]))
+# print(np.average(groupby_make_size.get_group(("TESLA", "FULL-SIZE"))["CITY (kWh/100 km)"]))
+
+# dataFrame_car.set_index("YEAR", inplace=True)
+groupby_year_make = dataFrame_car.groupby(["YEAR", "Make"])
+
+print(groupby_year_make.get_group((2013, "FORD")))
